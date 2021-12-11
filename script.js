51becef604a92a06ed4734ac97005cdf3806d63f
YarikDolbiev1 <raisindolbiev@gmail.com>
2021-12-11 10:27:55 +0200

    change

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
 function shuffle(arr) {
-    let rand, temp;
-    for (let i = 0; i < arr.length; i++) {
-      rand = Math.floor(Math.random() * (i + 1));
-      temp = arr[rand];
-      arr[rand] = arr[i];
-      arr[i] = temp;
-    }
-    return arr;
+  let rand, temp;
+  for (let i = 0; i < arr.length; i++) {
+    rand = Math.floor(Math.random() * (i + 1));
+    temp = arr[rand];
+    arr[rand] = arr[i];
+    arr[i] = temp;
   }
+  return arr;
+}
+shuffle(values);
+alert(values);
 
-  values = shuffle(values);
-  alert(values);
-   function play(values) {
-       let turn = 0
-       let card;
-       while ( values.length > 0){
-           turn ++;
-           if (turn % 2 != 0 ){
-               alert("You pick card");
-               card = values.pop();
-            
-
-           }
-               else {
-                   alert("I pick card");
-               
-               }
-               alert(card);
-               if (card == "Q"){
-                   turn = true;
-                   
-                   alert("You Win!")
-
-               }
-               else{
-                   turn = false;
-                   alert("Берите еще");
-               }
-
-           }
-
-
-       }
-       play(values);
+function play(values) {
+  let turn = 0;
+  let card;
+  while (values.length > 0) {
+    turn++;
+    if (turn % 2 != 0) {
+      alert("You pick card!");
+      card = values.pop();
+    } else {
+      alert("I pick card!");
+      card = values.pop();
+    }
+    alert(card);
+    if(card == "Q"){
+        if(turn=="Q"){
+            alert("you lose");
+            break;
+        }else{
+            alert("you win")
+            break;
+        }
+    }
+  }
+}
+play(values);
+       
    
